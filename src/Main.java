0c392000dd2a58d7648db7364cc1c53df273f635
Fabien Rousset <fabien.rousset@icloud.com>
2024-12-05 14:24:47 +0100

    création des logs

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,24 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.IOException;
+import java.util.logging.Level;
+
+import static logging.MonLogger.LOGGER;
+import static logging.MonLogger.initialiserLogger;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        //Initialisation du logger pour le programme
+        try {
+            initialiserLogger();
+        } catch (IOException ex) {
+            System.out.println("Erreur lors de l'initialisation du logger : " + ex.getMessage());
+            System.exit(1);
         }
+        LOGGER.log(Level.INFO, "Lancement de l'application");
+
+
+//        LOGGER.log(Level.INFO, "L'application a été quittée");
     }
 }
