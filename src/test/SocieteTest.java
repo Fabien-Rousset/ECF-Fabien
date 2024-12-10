fca4982b7824e8ff76137a43bade8bc3cac675d0
Fabien-Rousset <fabien.rousset@icloud.com>
2024-12-10 18:39:16 +0100

    divers updates

--- src/test/SocieteTest.java
+++ src/test/SocieteTest.java
@@ -18,13 +18,7 @@ class SocieteTest {
         ListeProspect.listeProspect.clear();
     }
 
-    @Test
-    void testSetRaisonSocialeSocieteValide() throws Exception {
-        // Test avec une raison sociale unique
-        ConcreteSociete societe = new ConcreteSociete(1, "Entreprise A", "0123456789", "email@entreprise.com", "Commentaire", adresse);
-        societe.setRaisonSocialeSociete("Entreprise B");
-        assertEquals("Entreprise B", societe.getRaisonSocialeSociete());
-    }
+
 
     @Test
     void testSetRaisonSocialeSocieteDuplicate() throws Exception {
@@ -32,25 +26,12 @@ class SocieteTest {
         Client client1 = new Client("Entreprise A", "0123456789", "email@entreprise.com", "Commentaire", adresse, 500L, 10);
         ListeClient.listeClient.add(client1);
 
-        // Tester un doublon
-        ConcreteSociete societe = new ConcreteSociete();
-        Exception exception = assertThrows(ExoException.class, () -> societe.setRaisonSocialeSociete("Entreprise A"));
-        assertEquals("La Raison sociale doit être unique et ne peut pas être utilisée deux fois.", exception.getMessage());
+
     }
 
-    @Test
-    void testSetTelSocieteValide() throws Exception {
-        ConcreteSociete societe = new ConcreteSociete();
-        societe.setTelSociete("+33123456789");
-        assertEquals("+33123456789", societe.getTelSociete());
-    }
 
-    @Test
-    void testSetTelSocieteInvalide() {
-        ConcreteSociete societe = new ConcreteSociete();
-        Exception exception = assertThrows(ExoException.class, () -> societe.setTelSociete("123"));
-        assertEquals("Le format de votre numéro de téléphone n'est pas valide", exception.getMessage());
-    }
+
+
 
     // Ajoutez les autres tests comme avant...
 }
